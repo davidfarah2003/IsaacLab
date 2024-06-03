473c4903208ca26e90d58a86b5da6add82b0b002
David Farah <davidfarah2003@gmail.com>
2024-06-03 07:34:31 +0200

    Add cube and action space

---
+++ source/standalone/my_project/base_env_setup.py
@@ -0,0 +1,299 @@
+import argparse
+import os
+
+from omni.isaac.lab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Tutorial on using the interactive scene interface.")
+parser.add_argument("--num_envs", type=int, default=2, help="Number of environments to spawn.")
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import torch
+
+import omni.isaac.lab.envs.mdp as mdp
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.assets import AssetBaseCfg, RigidObject, RigidObjectCfg
+from omni.isaac.lab.envs import ManagerBasedEnv, ManagerBasedEnvCfg
+from omni.isaac.lab.managers import ActionTerm, ActionTermCfg
+from omni.isaac.lab.managers import EventTermCfg as EventTerm
+from omni.isaac.lab.managers import ObservationGroupCfg as ObsGroup
+from omni.isaac.lab.managers import ObservationTermCfg as ObsTerm
+from omni.isaac.lab.managers import SceneEntityCfg
+from omni.isaac.lab.scene import InteractiveSceneCfg
+from omni.isaac.lab.utils import configclass
+from omni.isaac.lab.sensors import CameraCfg
+from omni.isaac.lab.sim import GroundPlaneCfg, UsdFileCfg
+
+@configclass
+class MySceneCfg(InteractiveSceneCfg):
+    """Configuration for the terrain scene with a legged robot."""
+
+    ground = AssetBaseCfg(prim_path="/World/ground", spawn=GroundPlaneCfg())
+
+    # add cube
+    cube: RigidObjectCfg = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/robot",
+        spawn=sim_utils.CuboidCfg(
+            size=(0.70, 0.31, 0.40),
+            rigid_props=sim_utils.RigidBodyPropertiesCfg(max_depenetration_velocity=1.0,
+                                                         max_linear_velocity=3,
+                                                         max_angular_velocity=80,
+                                                         disable_gravity=False),
+            mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+            physics_material=sim_utils.RigidBodyMaterialCfg(),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.5, 0.0)),
+        ),
+        init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.6152), lin_vel=(0,0,0)),
+    )
+
+    room = AssetBaseCfg(prim_path="{ENV_REGEX_NS}/room",
+                        spawn=UsdFileCfg(usd_path="omniverse://localhost/Library/assets/simple_room/simple_room.usd"),
+                        init_state=AssetBaseCfg.InitialStateCfg(pos=(0, 0, 0.8)))
+
+    ball = RigidObjectCfg(prim_path="{ENV_REGEX_NS}/ball",
+                          spawn=sim_utils.SphereCfg(radius=0.1,
+                                                    rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+                                                    mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+                                                    collision_props=sim_utils.CollisionPropertiesCfg(),
+                                                    visual_material=sim_utils.PreviewSurfaceCfg(
+                                                        diffuse_color=(1.0, 0.0, 0.0), metallic=0),
+                                                    ),
+                          init_state=RigidObjectCfg.InitialStateCfg(pos=(1, 1, 0.8)))
+
+    # sensors
+    camera = CameraCfg(
+        prim_path="{ENV_REGEX_NS}/robot/front_cam",
+        update_period=0.1,
+        height=480,
+        width=640,
+        data_types=["rgb", "distance_to_image_plane"],
+        spawn=sim_utils.PinholeCameraCfg(
+            focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
+        ),
+        offset=CameraCfg.OffsetCfg(pos=(0.510, 0.0, 0.015), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
+    )
+
+    # lights
+    light = AssetBaseCfg(
+        prim_path="/World/light",
+        spawn=sim_utils.DistantLightCfg(color=(0.75, 0.75, 0.75), intensity=3000.0),
+    )
+
+    sky_light = AssetBaseCfg(
+        prim_path="/World/skyLight",
+        spawn=sim_utils.DomeLightCfg(intensity=1000.0),
+    )
+
+class CubeActionTerm(ActionTerm):
+    """Simple action term that implements a PD controller to track a target position.
+
+    The action term is applied to the cube asset. It involves two steps:
+
+    1. **Process the raw actions**: Typically, this includes any transformations of the raw actions
+       that are required to map them to the desired space. This is called once per environment step.
+    2. **Apply the processed actions**: This step applies the processed actions to the asset.
+       It is called once per simulation step.
+
+    In this case, the action term simply applies the raw actions to the cube asset. The raw actions
+    are the desired target positions of the cube in the environment frame. The pre-processing step
+    simply copies the raw actions to the processed actions as no additional processing is required.
+    The processed actions are then applied to the cube asset by implementing a PD controller to
+    track the target position.
+    """
+
+    _asset: RigidObject
+    """The articulation asset on which the action term is applied."""
+
+    def __init__(self, cfg, env: ManagerBasedEnv):
+        # call super constructor
+        super().__init__(cfg, env)
+        # create buffers
+        self._raw_actions = torch.zeros(env.num_envs, 3, device=self.device)
+        self._processed_actions = torch.zeros(env.num_envs, 3, device=self.device)
+        self._vel_command = torch.zeros(self.num_envs, 6, device=self.device)
+        # gains of controller
+        self.p_gain = cfg.p_gain
+        self.d_gain = cfg.d_gain
+
+    """
+    Properties.
+    """
+
+    @property
+    def action_dim(self) -> int:
+        return self._raw_actions.shape[1]
+
+    @property
+    def raw_actions(self) -> torch.Tensor:
+        return self._raw_actions
+
+    @property
+    def processed_actions(self) -> torch.Tensor:
+        return self._processed_actions
+
+    """
+    Operations
+    """
+
+    def process_actions(self, actions: torch.Tensor):
+        # store the raw actions
+        self._raw_actions[:] = actions
+        # Apply actions once the robot hits the ground
+        if torch.all(self._asset.data.root_lin_vel_w[:, 2].abs() < 1e-2):
+            self._processed_actions[:, :2] = self._raw_actions[:, :2]
+            self._processed_actions[:, 2] = 0.0
+        else:
+            self._processed_actions = torch.zeros_like(self._processed_actions)
+
+    def apply_actions(self):
+        # implement a PD controller to track the target position
+        pos_error = self._processed_actions - (self._asset.data.root_pos_w - self._env.scene.env_origins)
+        vel_error = -self._asset.data.root_lin_vel_w
+        # set velocity targets
+        # set velocity targets only for x and y components
+        self._vel_command[:, :2] = self.p_gain * pos_error[:, :2] + self.d_gain * vel_error[:, :2]
+        self._vel_command[:, 2] = self._asset.data.root_lin_vel_w[:, 2]  # Maintain current z velocity
+
+        self._asset.write_root_velocity_to_sim(self._vel_command)
+
+@configclass
+class CubeActionTermCfg(ActionTermCfg):
+    """Configuration for the cube action term."""
+
+    class_type: type = CubeActionTerm
+    """The class corresponding to the action term."""
+
+    p_gain: float = 5.0
+    """Proportional gain of the PD controller."""
+    d_gain: float = 0.5
+    """Derivative gain of the PD controller."""
+
+
+@configclass
+class ActionsCfg:
+    joint_pos = CubeActionTermCfg(asset_name="cube")
+
+
+##
+# Custom observation term
+##
+def base_position(env: ManagerBasedEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Root linear velocity in the asset's root frame."""
+    # extract the used quantities (to enable type-hinting)
+    asset: RigidObject = env.scene[asset_cfg.name]
+    return asset.data.root_pos_w - env.scene.env_origins
+
+@configclass
+class ObservationsCfg:
+    """Observation specifications for the MDP."""
+    @configclass
+    class PolicyCfg(ObsGroup):
+        """Observations for policy group."""
+
+        # cube velocity
+        position = ObsTerm(func=base_position, params={"asset_cfg": SceneEntityCfg("cube")})
+
+        def __post_init__(self):
+            self.enable_corruption = True
+            self.concatenate_terms = True
+
+    # observation groups
+    policy: PolicyCfg = PolicyCfg()
+
+@configclass
+class EventCfg:
+    reset_base = EventTerm(
+        func=mdp.reset_root_state_uniform,
+        mode="reset",
+        params={
+            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (0, 0)},
+            "velocity_range": {"x": (0, 0),"y": (0, 0),"z": (0, 0)},
+            "asset_cfg": SceneEntityCfg("cube"),
+        },
+    )
+
+
+@configclass
+class CubeEnvCfg(ManagerBasedEnvCfg):
+    """Configuration for the locomotion velocity-tracking environment."""
+
+    # Scene settings
+    scene: MySceneCfg = MySceneCfg(num_envs=args_cli.num_envs, env_spacing=10)
+    # Basic settings
+    actions: ActionsCfg = ActionsCfg()
+    events: EventCfg = EventCfg()
+    observations: ObservationsCfg = ObservationsCfg()
+
+    def __post_init__(self):
+        """Post initialization."""
+        # general settings
+        self.decimation = 4  # env decimation -> 50 Hz control
+        # simulation settings
+        self.sim.dt = 0.005  # simulation timestep -> 200 Hz physics
+
+
+def main():
+    """Main function."""
+    # setup base environment
+    env_cfg = CubeEnvCfg()
+    env = ManagerBasedEnv(cfg=env_cfg)
+
+    # Setup target position commands with random x and y, and fixed z
+    target_position = torch.rand(env.num_envs, 2, device=env.device)   # Randomize x and y coordinates
+    fixed_z_column = torch.full((env.num_envs, 1), 2, device=env.device)    # fixed z of 1m
+    target_position = torch.cat((target_position, fixed_z_column), dim=1)
+
+    # offset all targets so that they move to the world origin
+    #target_position -= env.scene.env_origins
+
+    # simulate physics
+    count = 0
+    obs, _ = env.reset()
+    while simulation_app.is_running():
+        with torch.inference_mode():
+            # reset
+            if count % 300 == 0:
+                count = 0
+                obs, _ = env.reset()
+                print("-" * 80)
+                print("[INFO]: Resetting environment...")
+
+            # step env
+            if(count < 100):
+                target_position[:, :2] = torch.tensor([1, 1], device=env.device)
+            elif (count < 150):
+                target_position[:, :2] = torch.tensor([1, -1], device=env.device)
+            elif (count < 200):
+                target_position[:, :2] = torch.tensor([-1, -1], device=env.device)
+            else:
+                target_position[:, :2] = torch.tensor([-1, 1], device=env.device)
+
+            obs, _ = env.step(target_position)
+            # print mean squared position error between target and current position
+            error = torch.norm(obs["policy"] - target_position).mean().item()
+            print(f"[Step: {count:04d}]: Mean position error: {error:.4f}")
+
+
+            # update counter
+            count += 1
+
+    # close the environment
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
---
+++ source/standalone/my_project/rl_env_setup.py
@@ -0,0 +1,329 @@
+import argparse
+import math
+import os
+
+from omni.isaac.orbit.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Tutorial on using the interactive scene interface.")
+parser.add_argument("--num_envs", type=int, default=2, help="Number of environments to spawn.")
+parser.add_argument("--task", type=str, default="Isaac-Velocity-Flat-Unitree-Go2-Play-v0", help="Name of the task.")
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import torch
+
+import omni.isaac.orbit.sim as sim_utils
+from omni.isaac.orbit.assets import ArticulationCfg, AssetBaseCfg
+from omni.isaac.orbit.scene import InteractiveScene, InteractiveSceneCfg
+from omni.isaac.orbit.sim import SimulationContext, UsdFileCfg, GroundPlaneCfg, SpawnerCfg
+from omni.isaac.orbit.utils import configclass
+from omni.isaac.orbit.envs import BaseEnvCfg, BaseEnv, mdp
+
+##
+# Pre-defined configs
+##
+from omni.isaac.orbit_assets import CARTPOLE_CFG, UNITREE_GO2_CFG  # isort:skip
+from omni.isaac.orbit.sensors import RayCasterCfg, ContactSensorCfg, CameraCfg
+from omni.isaac.orbit.sensors.ray_caster import patterns
+from omni.isaac.orbit.envs import RLTaskEnvCfg
+from omni.isaac.orbit.utils import configclass
+from omni.isaac.orbit.envs import RLTaskEnvCfg
+import omni.isaac.orbit.sim as sim_utils
+from omni.isaac.orbit.assets import ArticulationCfg, AssetBaseCfg
+from omni.isaac.orbit.scene import InteractiveSceneCfg
+from omni.isaac.orbit.sensors import ContactSensorCfg, RayCasterCfg, patterns
+from omni.isaac.orbit.terrains import TerrainImporterCfg
+from omni.isaac.orbit.utils import configclass
+from omni.isaac.orbit_assets.unitree import UNITREE_GO2_CFG, GO1_ACTUATOR_CFG
+from omni.isaac.orbit.managers import EventTermCfg as EventTerm
+from omni.isaac.orbit.managers import ObservationGroupCfg as ObsGroup
+from omni.isaac.orbit.managers import ObservationTermCfg as ObsTerm
+from omni.isaac.orbit.managers import RewardTermCfg as RewTerm
+from omni.isaac.orbit.managers import SceneEntityCfg
+from omni.isaac.orbit.managers import TerminationTermCfg as DoneTerm
+from omni.isaac.orbit.utils import configclass
+from omni.isaac.orbit.utils.noise import AdditiveUniformNoiseCfg as Unoise
+import omni.isaac.orbit_tasks.locomotion.velocity.mdp as mdp
+from omni.isaac.orbit.utils.assets import ISAAC_NUCLEUS_DIR, ISAAC_ORBIT_NUCLEUS_DIR, check_file_path, read_file
+import gymnasium as gym
+from omni.isaac.orbit_tasks.locomotion.velocity.config.unitree_go2.agents.rsl_rl_cfg import UnitreeGo2FlatPPORunnerCfg
+from omni.isaac.orbit_tasks.utils import get_checkpoint_path
+from omni.isaac.orbit_tasks.utils.wrappers.rsl_rl import RslRlVecEnvWrapper
+from rsl_rl.runners import OnPolicyRunner
+import omni.isaac.orbit_tasks.locomotion.velocity.config.unitree_go2
+
+@configclass
+class MySceneCfg(InteractiveSceneCfg):
+    """Configuration for the terrain scene with a legged robot."""
+
+    ground = AssetBaseCfg(prim_path="/World/ground", spawn=GroundPlaneCfg())
+
+    room = AssetBaseCfg(prim_path="{ENV_REGEX_NS}/room",
+                        spawn=UsdFileCfg(usd_path="omniverse://localhost/Library/assets/simple_room/simple_room.usd"),
+                        init_state=AssetBaseCfg.InitialStateCfg(pos=(0, 0, 0.8)))
+
+    # robots
+    robot: ArticulationCfg = UNITREE_GO2_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot",
+                                                     init_state=ArticulationCfg.InitialStateCfg(
+                                                         pos=(0.0, 0.0, 0.4),
+                                                         joint_pos={
+                                                             ".*L_hip_joint": 0.1,
+                                                             ".*R_hip_joint": -0.1,
+                                                             "F[L,R]_thigh_joint": 0.8,
+                                                             "R[L,R]_thigh_joint": 1.0,
+                                                             ".*_calf_joint": -1.5,
+                                                         },
+                                                         joint_vel={".*": 0.0},
+                                                     )
+                                                     )
+
+    height_scanner = RayCasterCfg(
+        prim_path="{ENV_REGEX_NS}/Robot/base",
+        update_period=0.02,
+        offset=RayCasterCfg.OffsetCfg(pos=(0.0, 0.0, 20.0)),
+        attach_yaw_only=True,
+        pattern_cfg=patterns.GridPatternCfg(resolution=0.1, size=[1.6, 1.0]),
+        debug_vis=False,
+        mesh_prim_paths=["/World/ground"],
+    )
+
+    # sensors
+    camera = CameraCfg(
+        prim_path="{ENV_REGEX_NS}/Robot/base/front_cam",
+        update_period=0.1,
+        height=480,
+        width=640,
+        data_types=["rgb", "distance_to_image_plane"],
+        spawn=sim_utils.PinholeCameraCfg(
+            focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
+        ),
+        offset=CameraCfg.OffsetCfg(pos=(0.510, 0.0, 0.015), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
+    )
+
+    contact_forces = ContactSensorCfg(prim_path="{ENV_REGEX_NS}/Robot/.*", history_length=3, track_air_time=True)
+
+    # lights
+    light = AssetBaseCfg(
+        prim_path="/World/light",
+        spawn=sim_utils.DistantLightCfg(color=(0.75, 0.75, 0.75), intensity=3000.0),
+    )
+
+    sky_light = AssetBaseCfg(
+        prim_path="/World/skyLight",
+        spawn=sim_utils.DomeLightCfg(intensity=1000.0,
+                                     texture_file="omniverse://localhost/NVIDIA/Assets/Skies/Cloudy/abandoned_parking_4k.hdr"),
+    )
+
+
+@configclass
+class ActionsCfg:
+    """Action specifications for the MDP."""
+    joint_pos = mdp.JointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.5, use_default_offset=True)
+
+
+def constant_commands(env: BaseEnv) -> torch.Tensor:
+    """The generated command from the command generator."""
+    return torch.tensor([[1, 0, 0]], device=env.device).repeat(env.num_envs, 1)
+
+
+@configclass
+class ObservationsCfg:
+    """Observation specifications for the MDP."""
+    @configclass
+    class PolicyCfg(ObsGroup):
+        """Observations for policy group."""
+
+        # observation terms (order preserved)
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel)
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel)
+        projected_gravity = ObsTerm(
+            func=mdp.projected_gravity,
+            noise=Unoise(n_min=-0.05, n_max=0.05),
+        )
+        velocity_commands = ObsTerm(func=constant_commands)
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel)
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
+        actions = ObsTerm(func=mdp.last_action)
+        height_scan = ObsTerm(
+            func=mdp.height_scan,
+            params={"sensor_cfg": SceneEntityCfg("height_scanner")},
+            clip=(-1.0, 1.0),
+        )
+
+        def __post_init__(self):
+            self.enable_corruption = True
+            self.concatenate_terms = True
+
+    # observation groups
+    policy: PolicyCfg = PolicyCfg()
+
+
+@configclass
+class EventCfg:
+    """Configuration for events."""
+
+    reset_scene = EventTerm(func=mdp.reset_scene_to_default, mode="reset")
+
+
+@configclass
+class CommandsCfg:
+    """Command specifications for the MDP."""
+    base_velocity = mdp.UniformVelocityCommandCfg(
+        asset_name="robot",
+        resampling_time_range=(0.0, 0.0),
+        rel_standing_envs=0.02,
+        rel_heading_envs=1.0,
+        heading_command=True,
+        heading_control_stiffness=0.5,
+        debug_vis=True,
+        ranges=mdp.UniformVelocityCommandCfg.Ranges(
+            lin_vel_x=(0.0, 0.0), lin_vel_y=(0.0, 0.0), ang_vel_z=(0.0, 0.0), heading=(0, 0)
+        ),
+    )
+
+
+@configclass
+class TerminationsCfg:
+    """Termination terms for the MDP."""
+    time_out = DoneTerm(func=mdp.time_out, time_out=True)
+    base_contact = DoneTerm(
+        func=mdp.illegal_contact,
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names="base"), "threshold": 1.0},
+    )
+
+
+@configclass
+class RewardsCfg:
+    """Reward terms for the MDP."""
+
+    # -- task
+    track_lin_vel_xy_exp = RewTerm(
+        func=mdp.track_lin_vel_xy_exp, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
+    )
+    track_ang_vel_z_exp = RewTerm(
+        func=mdp.track_ang_vel_z_exp, weight=0.5, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
+    )
+    # -- penalties
+    lin_vel_z_l2 = RewTerm(func=mdp.lin_vel_z_l2, weight=-2.0)
+    ang_vel_xy_l2 = RewTerm(func=mdp.ang_vel_xy_l2, weight=-0.05)
+    dof_torques_l2 = RewTerm(func=mdp.joint_torques_l2, weight=-1.0e-5)
+    dof_acc_l2 = RewTerm(func=mdp.joint_acc_l2, weight=-2.5e-7)
+    action_rate_l2 = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
+    feet_air_time = RewTerm(
+        func=mdp.feet_air_time,
+        weight=0.125,
+        params={
+            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*foot"),
+            "command_name": "base_velocity",
+            "threshold": 0.5,
+        },
+    )
+    undesired_contacts = RewTerm(
+        func=mdp.undesired_contacts,
+        weight=-1.0,
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*thigh"), "threshold": 1.0},
+    )
+    # -- optional penalties
+    flat_orientation_l2 = RewTerm(func=mdp.flat_orientation_l2, weight=0.0)
+    dof_pos_limits = RewTerm(func=mdp.joint_pos_limits, weight=0.0)
+
+
+@configclass
+class QuadrupedEnvCfg(RLTaskEnvCfg):
+    """Configuration for the locomotion velocity-tracking environment."""
+
+    # Scene settings
+    scene: MySceneCfg = MySceneCfg(num_envs=args_cli.num_envs, env_spacing=20)
+    # Basic settings
+    observations: ObservationsCfg = ObservationsCfg()
+    actions: ActionsCfg = ActionsCfg()
+    commands: CommandsCfg = CommandsCfg()
+    # RL settings
+    rewards: RewardsCfg = RewardsCfg()
+    terminations: TerminationsCfg = TerminationsCfg()
+    events: EventCfg = EventCfg()
+
+
+    def __post_init__(self):
+        """Post initialization."""
+        # general settings
+        self.decimation = 4  # env decimation -> 50 Hz control
+        # simulation settings
+        self.sim.dt = 0.005  # simulation timestep -> 200 Hz physics
+
+        # update sensor update periods
+        # we tick all the sensors based on the smallest update period (physics update period)
+        if self.scene.height_scanner is not None:
+            self.scene.height_scanner.update_period = self.decimation * self.sim.dt  # 50 Hz
+
+
+def main():
+    """Main function."""
+    # setup base environment
+    env_cfg = QuadrupedEnvCfg()
+
+    agent_cfg = UnitreeGo2FlatPPORunnerCfg()
+    # create isaac environment
+    env = gym.make(args_cli.task, cfg=env_cfg)
+    # wrap around environment for rsl-rl
+    env = RslRlVecEnvWrapper(env)
+
+    # specify directory for logging experiments
+    log_root_path = os.path.join("logs", "rsl_rl", agent_cfg["experiment_name"])
+    log_root_path = os.path.abspath(log_root_path)
+    print(f"[INFO] Loading experiment from directory: {log_root_path}")
+
+    resume_path = get_checkpoint_path(log_root_path, agent_cfg["load_run"], agent_cfg["load_checkpoint"])
+    print(f"[INFO]: Loading model checkpoint from: {resume_path}")
+
+    # load previously trained model
+    ppo_runner = OnPolicyRunner(env, agent_cfg, log_dir=None, device=agent_cfg["device"])
+    ppo_runner.load(resume_path)
+    print(f"[INFO]: Loading model checkpoint from: {resume_path}")
+
+    # obtain the trained policy for inference
+    policy = ppo_runner.get_inference_policy(device=env.unwrapped.device)
+
+    # reset environment
+    obs, _ = env.get_observations()
+
+    # simulate physics
+    count = 0
+    obs, _ = env.reset()
+    while simulation_app.is_running():
+        with torch.inference_mode():
+            # reset
+            if count % 1000 == 0:
+                obs, _ = env.reset()
+                count = 0
+                print("-" * 80)
+                print("[INFO]: Resetting environment...")
+
+            # agent stepping
+            actions = policy(obs)
+
+            # env stepping
+            obs, _, _, _ = env.step(actions)
+
+            # update counter
+            count += 1
+
+    # close the environment
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
